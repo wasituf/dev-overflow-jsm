d072f3fd8222fc2442282cfdfc04669309af3019
wasituf <wasit.allin1@gmainl.com>
2024-01-13 02:06:44 +0600

    Changed eslint rules

--- components/shared/navbar/Theme.tsx
+++ components/shared/navbar/Theme.tsx
@@ -9,8 +9,6 @@ import {
   MenubarContent,
   MenubarItem,
   MenubarMenu,
-  MenubarSeparator,
-  MenubarShortcut,
   MenubarTrigger,
 } from '@/components/ui/menubar'
 import Image from 'next/image'
